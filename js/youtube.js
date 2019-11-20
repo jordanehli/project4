0d7aec23752784683cc7e6090394aa19d730fb76
jordanehli <jpgehlinger@gmail.com>
2019-11-19 21:32:47 -0500

    editing styles and youtube

--- js/youtube.js
+++ js/youtube.js
@@ -1,10 +1,4 @@
-  /**
-   * Sample JavaScript code for youtube.search.list
-   * See instructions for running APIs Explorer code samples locally:
-   * https://developers.google.com/explorer-help/guides/code_samples#javascript
-   */
-
-  function authenticate() {
+function authenticate() {
     return gapi.auth2.getAuthInstance()
         .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
         .then(function() { console.log("Sign-in successful"); },
@@ -20,12 +14,12 @@
   function execute() {
     return gapi.client.youtube.search.list({
       "part": "snippet",
-      "maxResults": 10,
-      "q": "zion national park"
+      "q": "#zion national park",
     })
         .then(function(response) {
                 // Handle the results here (response.result has the parsed body).
                 console.log("Response", response);
+                parseVideos(response);
               },
               function(err) { console.error("Execute error", err); });
   }
@@ -33,19 +27,20 @@
     gapi.auth2.init({client_id: "78599238566-cc5d6q365fl532409jtf667lvl094d8j.apps.googleusercontent.com"});
   });
 
+
   function parseVideos(response){
-      var html = "";
-      var tempPath = response.result.items;
-      console.log(response.result.etag);
-      console.log(response.result.items[0].snippet.title);
-      console.log(tempPath[0].snippet.thumbnails.default.url);
+    var html = "";
+    var tempPath = response.result.items;
+    console.log(response.result.etag);
+    console.log(response.result.items[0].snippet.title);
+    console.log(tempPath[0].snippet.thumbnails.default.url);
 
-      for (var i = 0, len = response.result.items.length; i < len; ++i) {
-        console.log("item " + i);
-        html += '<h2>' + tempPath[i].snippet.title + '</h2>';
-        html += '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + tempPath[i].id.videoId + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-
-      }
-      document.getElementById("results").innerHTML = html;
+    for (var i = 0, len = response.result.items.length; i < len; ++i) {
+      console.log("item " + i);
+      html += '<h2>' + tempPath[i].snippet.title + '</h2>';
+      html += '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + tempPath[i].id.videoId + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
 
     }
+    document.getElementById("results").innerHTML = html;
+
+  }
